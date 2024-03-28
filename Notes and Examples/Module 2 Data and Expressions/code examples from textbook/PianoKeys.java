cdfba90463c152af193b64f93f4a800079bb2e53
average humun <113070026+averagehumun@users.noreply.github.com>
2024-03-28 11:18:12 -0600

    Add files via upload

---
+++ Notes and Examples/Module 2 Data and Expressions/code examples from textbook/PianoKeys.java
@@ -0,0 +1,15 @@
+//********************************************************************
+//  PianoKeys.java      Java Foundations
+//
+//  Demonstrates the declaration, initialization, and use of an
+//  integer variable.
+//********************************************************************
+
+public class PianoKeys {
+
+    public static void main (String[] arg) {
+        int keys = 88;
+
+        System.out.println("A piano has " + keys + " keys.");
+    }
+}
